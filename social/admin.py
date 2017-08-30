638aa87b92e0edc39cba412cda343380e106c8ea
Tony Mugendi <mugendi.tony26@gmail.com>
2017-08-30 21:42:42 +0300

    Custom Admin panel

--- social/admin.py
+++ social/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from social.models import *
 
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+	list_display = ('user', 'user_info', 'city', 'phone', 'website')
+
+	def user_info(self, obj):
+		return obj.description
+
+	def get_queryset(self, request):
+		queryset = super(UserProfileAdmin, self).get_queryset(request)
+		queryset = queryset.order_by('-phone', 'user')
+		return queryset
+
 # Register your models here.
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, UserProfileAdmin)
+	
+
+
+
