0ec02739c837c72b0ec93703a87aaa2b5f0e7cb0
Tony Mugendi <mugendi.tony26@gmail.com>
2017-09-02 22:31:22 +0300

    Many to many relationship

--- home/models.py
+++ home/models.py
@@ -7,4 +7,9 @@ class Post(models.Model):
 	user = models.ForeignKey(User)
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
+
+class Friend(models.Model):
+	users = models.ManyToManyField(User)
+	
+		
 	
--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from home.forms import *
 from home.models import *
+
 
 class HomeView(TemplateView):
 	template_name = 'home/home.html'
